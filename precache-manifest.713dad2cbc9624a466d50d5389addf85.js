16fff60379f860a576952965fc0997ce0b8ecfa8
Bilalmalik1561 <52446260+Bilalmalik1561@users.noreply.github.com>
2019-09-21 13:56:05 +0530

    Add files via upload

---
+++ precache-manifest.713dad2cbc9624a466d50d5389addf85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ff6f11358bd99b7e4b134a7761236e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1748d7252a4440ec7d35",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9967d8f7b5c28a11572f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1748d7252a4440ec7d35",
+    "url": "/static/js/2.0392c410.chunk.js"
+  },
+  {
+    "revision": "9967d8f7b5c28a11572f",
+    "url": "/static/js/main.02d56c4d.chunk.js"
+  },
+  {
+    "revision": "47abdb54b117d333d326",
+    "url": "/static/js/runtime~main.f7e395dc.js"
+  }
+]);
